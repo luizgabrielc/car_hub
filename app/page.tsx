44415751b55aa38e5b25a5191b5d937bc7657e1c
Luiz Gabriel <luizgabriel1233@gmail.com>
2023-07-07 17:16:00 -0300

    feat: fetch API cars

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,29 @@
-"use client"
+"use client";
 import { CustomFilter, Hero, SearchBar } from "@/components";
+// import { fetchCars } from "@/utils";
+import { useEffect } from "react";
 
-export default function Home() {
+export default async function Home() {
+  // useEffect(() => {
+  //   fetchCars().then((allCars) => console.log("allCars", allCars)).catch(console.log);
+  // }, []);
   return (
     <main className="overflow-hidden">
-      <Hero/>
+      <Hero />
       <div className="mt-12 padding-x padding-y max-width" id="discover">
         <div className="home__text-container">
-          <h1 className="text-4xl font-extrabold">
-            Car Catalogue
-          </h1>
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
           <p>Explore the cars you might like</p>
         </div>
         <div className="home__filters">
-          <SearchBar/>
+          <SearchBar />
 
           <div className="home__filter-container">
-            <CustomFilter/>
-            <CustomFilter/>
+            <CustomFilter />
+            <CustomFilter />
           </div>
         </div>
       </div>
     </main>
-  )
+  );
 }
